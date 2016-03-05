2ade5b30dee9277e8bd32647bdab86bae5b11374
Shuhao Wu <shuhao@shuhaowu.com>
2016-03-05 12:16:56 -0500

    More utilities function

--- __init__.py
+++ __init__.py
@@ -34,3 +34,13 @@ def cd(newdir):
         yield
     finally:
         os.chdir(prevdir)
+
+
+def mkdir_p(path):
+  try:
+    os.makedirs(path)
+  except OSError as e:
+    if e.errno == errno.EEXIST and os.path.isdir(path):
+      pass
+    else:
+      raise
